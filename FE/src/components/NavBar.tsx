5d36e0e74689b02f611abcba776bacb4314eb610
Felicio Angga <felicioangga004@gmail.com>
2023-01-28 23:54:01 +0700

    object cover on nav bar image

--- FE/src/components/NavBar.tsx
+++ FE/src/components/NavBar.tsx
@@ -42,7 +42,7 @@ function NavBar() {
       <div onClick={changePass} className='cursor-pointer absolute flex gap-2 right-0'>
         <div></div>
         <p className='font-medium text-xl my-auto'>{username}</p>
-        <img className='w-12 h-12 rounded-full' src={image64 ? `data:image/png;base64,${image64}` : 'https://assets.pikiran-rakyat.com/crop/0x0:0x0/x/photo/2022/08/11/1722136995.jpg'} alt="" />
+        <img className='object-cover w-12 h-12 rounded-full' src={image64 ? `data:image/png;base64,${image64}` : 'https://assets.pikiran-rakyat.com/crop/0x0:0x0/x/photo/2022/08/11/1722136995.jpg'} alt="" />
       </div>
       <img onClick={() => navigate('/')} className="w-36" src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2a/Instagram_logo.svg/800px-Instagram_logo.svg.png?20160616034027" alt="" />
     </div>
